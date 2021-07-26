7c97273071b6753c196b2247ef8519a3dd80eefc
neethunsr <neethu2001nsr@gmail.com>
2021-07-26 18:45:50 +0530

    build widget added

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Center(
-            child: Text('CoviLog'),
-          ),
-          backgroundColor: Colors.blue[900],
-        ),
-        body: Center(
-          child: Image(
-          image: AssetImage('images/coviscan.png'),
-          ),
-        ) 
-      )
-    )
-  );
+  runApp(CoviLog());
+}
+
+class CoviLog extends StatelessWidget {
+  const CoviLog({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: Center(
+                child: Text('CoviLog'),
+              ),
+              backgroundColor: Colors.blue[900],
+            ),
+            body: Center(
+              child: Image(
+                image: AssetImage('images/coviscan.png'),
+              ),
+            )));
+  }
 }
